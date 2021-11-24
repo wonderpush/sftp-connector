fa91bc5d7e7b2e89775b79557b22851580684897
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2021-11-24 16:42:50 +0400

    [Feat] parseDatasFromCsv :sparkles: store the remote .csv file in local directory before processing

--- helpers/parseDatasFromCsv.js
+++ helpers/parseDatasFromCsv.js
@@ -1,53 +1,82 @@
 // DEPENDENCIES
+import Client from "ssh2-sftp-client";
 import fs from "fs";
 import { parse } from "csv-parse/sync";
 
-const parseDatasFromCsv = path => {
-	const input = fs.readFileSync(path, process.env.CSV_PARSE_COLUMS || "utf8");
+// HELPERS
+import setLogs from "./setLogs.js";
 
-	const records = parse(input, {
-		columns: process.env.CSV_PARSE_COLUMS || true,
-		comment: process.env.CSV_PARSE_COMMENT || "",
-		delimiter: process.env.CSV_PARSE_DELIMETER || ",",
-		encoding: process.env.CSV_PARSE_ENCODING || "utf8",
-		escape: process.env.CSV_PARSE_ESCAPE || '"',
-		quote: process.env.CSV_PARSE_QUOTE || '"',
-		record_delimiter: process.env.CSV_PARSE_RECORD_DELIMITER || [],
-		skip_empty_lines: process.env.CSV_PARSE_SKIP_EMPTY_LINES || true
-	});
+const parseDatasFromCsv = async (path, query) => {
+	const sftp = new Client();
 
-	const notificationParamsColumns = Object.keys(records[0]).filter(column => {
-		return (
-			column !== "nom" &&
-			column !== "prenom" &&
-			column !== (process.env.CSV_COLUMN_USER_ID || "user_id") &&
-			column !== (process.env.CSV_COLUMN_CAMPAIGN_ID || "campaign_id")
-		);
-	});
+	const config = {
+		host: process.env.FTP_HOST,
+		port: process.env.FTP_PORT,
+		username: process.env.FTP_USER,
+		privateKey: fs.readFileSync(process.env.FTP_PRIVATE_KEY)
+	};
 
-	const query = {};
+	let remotePath = path;
 
-	query.accessToken = process.env.WP_ACCESS_TOKEN;
+	let tmpPath = "./.tmp.csv";
 
-	query.targetUserIds = records.map(
-		item => item[process.env.CSV_COLUMN_USER_ID || "user_id"]
-	);
-	query.campaignId =
-		records[0][process.env.CSV_COLUMN_CAMPAIGN_ID || "campaign_id"];
+	await sftp
+		.connect(config)
+		.then(() => {
+			return sftp.get(remotePath, tmpPath);
+		})
+		.then(() => {
+			const input = fs.readFileSync(
+				tmpPath,
+				process.env.CSV_PARSE_ENCODING || "utf8"
+			);
 
-	if (notificationParamsColumns.length > 0) {
-		query.notificationParams = records.map(row => {
-			const customParams = {};
+			setLogs(input);
 
-			notificationParamsColumns.forEach(parameter => {
-				customParams[parameter] = row[parameter];
+			const records = parse(input, {
+				columns: process.env.CSV_PARSE_COLUMNS === "true" || true,
+				comment: process.env.CSV_PARSE_COMMENT || "",
+				delimiter: process.env.CSV_PARSE_DELIMETER || ",",
+				encoding: process.env.CSV_PARSE_ENCODING || "utf8",
+				escape: process.env.CSV_PARSE_ESCAPE || '"',
+				quote: process.env.CSV_PARSE_QUOTE || '"',
+				record_delimiter: [],
+				skip_empty_lines:
+					process.env.CSV_PARSE_SKIP_EMPTY_LINES === "true" || true
 			});
 
-			return customParams;
-		});
-	}
+			const notificationParamsColumns = Object.keys(records[0]).filter(
+				column => {
+					return (
+						column !== "nom" &&
+						column !== "prenom" &&
+						column !== (process.env.CSV_COLUMN_USER_ID || "user_id") &&
+						column !== (process.env.CSV_COLUMN_CAMPAIGN_ID || "campaign_id")
+					);
+				}
+			);
 
-	return query;
+			query.accessToken = process.env.WP_ACCESS_TOKEN;
+
+			query.targetUserIds = records.map(
+				item => item[process.env.CSV_COLUMN_USER_ID || "user_id"]
+			);
+			query.campaignId =
+				records[0][process.env.CSV_COLUMN_CAMPAIGN_ID || "campaign_id"];
+
+			if (notificationParamsColumns.length > 0) {
+				query.notificationParams = records.map(row => {
+					const customParams = {};
+
+					notificationParamsColumns.forEach(parameter => {
+						customParams[parameter] = row[parameter];
+					});
+
+					return customParams;
+				});
+			}
+		})
+		.then(() => sftp.end());
 };
 
 export default parseDatasFromCsv;
--- index.js
+++ index.js
@@ -1,50 +1,83 @@
+// ASSETS
+import logs from "./assets/logs.js";
+import fs from "fs";
+
 // DEPENDENCIES
 
 // HELPERS
+import setLogs from "./helpers/setLogs.js";
 import getFilesList from "./helpers/getFilesList.js";
+import parseDatasFromCsv from "./helpers/parseDatasFromCsv.js";
 
 let candidatesFiles = {};
 let lastListing = {};
 let newListing = {};
 
-getFilesList(newListing).then(() => {
+getFilesList(newListing, process.env.FTP_PATH).then(() => {
+	setLogs(logs.sshConnectedInfo);
+
+	Object.keys(newListing).forEach(fileName => {
+		candidatesFiles[fileName] = 1;
+
+		setLogs(logs.FileInfo, fileName);
+	});
+
 	setInterval(() => {
-		lastListing = {};
 		lastListing = { ...newListing };
 		newListing = {};
 
-		getFilesList(newListing).then(async () => {
-			// to check deleted files
-			Object.keys(lastListing).forEach(fileName => {
-				if (Object.keys(newListing).indexOf(fileName) < 0) {
-					delete candidatesFiles[fileName];
-					console.log("🚨 File removed : ", fileName);
-				}
-			});
-
-			Object.keys(newListing).forEach(fileName => {
-				// to check new files
-				if (Object.keys(lastListing).indexOf(fileName) < 0) {
-					candidatesFiles[fileName] = 1;
-					console.log("✨ New file detected : ", fileName);
-				} else {
-					// handle edited files
-					if (
-						newListing[fileName].modifyTime !== lastListing[fileName].modifyTime
-					) {
+		getFilesList(newListing, process.env.FTP_PATH)
+			.then(async () => {
+				// to check deleted files
+				Object.keys(lastListing).forEach(fileName => {
+					if (Object.keys(newListing).indexOf(fileName) < 0) {
 						delete candidatesFiles[fileName];
-					}
 
-					// handle non edited files
-					if (
-						newListing[fileName].modifyTime === lastListing[fileName].modifyTime
-					) {
-						candidatesFiles[fileName] = candidatesFiles[fileName]
-							? candidatesFiles[fileName] + 1
-							: 1;
+						setLogs(logs.fileDeletedInfo, fileName);
 					}
-				}
-			});
-		});
+				});
+
+				Object.keys(newListing).forEach(fileName => {
+					// to check new files
+					if (Object.keys(lastListing).indexOf(fileName) < 0) {
+						candidatesFiles[fileName] = 1;
+
+						setLogs(logs.newFileInfo, fileName);
+					} else {
+						// handle edited files
+						if (
+							newListing[fileName].modifyTime !==
+							lastListing[fileName].modifyTime
+						) {
+							delete candidatesFiles[fileName];
+						}
+
+						// handle non edited files
+						if (
+							newListing[fileName].modifyTime ===
+							lastListing[fileName].modifyTime
+						) {
+							candidatesFiles[fileName] = candidatesFiles[fileName]
+								? candidatesFiles[fileName] + 1
+								: 1;
+						}
+					}
+				});
+			})
+			.then(() => {
+				Object.keys(candidatesFiles).forEach(fileName => {
+					if (candidatesFiles[fileName] > process.env.STALE_FILE_CHECKS) {
+						delete candidatesFiles[fileName];
+
+						setLogs(logs.startFileProcessInfo, fileName);
+
+						let query = {};
+
+						parseDatasFromCsv(process.env.FTP_PATH + fileName, query);
+						// ).then(() => console.log(query));
+					}
+				});
+			})
+			.catch(error => setLogs(error));
 	}, process.env.LISTING_INTERVAL_MS);
 });
