546de02ba9ce5e0c0db71430a1fc4b3001f72d45
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2021-11-26 16:16:09 +0400

    [Perf] parseDataFromCsv :zap: optimize ram memory use

--- helpers/parseDataFromCsv.js
+++ helpers/parseDataFromCsv.js
@@ -23,65 +23,41 @@ const parseDataFromCsv = async (sftpConfig, remotePath) => {
 		})
 		.then(() => {
 			/* Store tmp file */
-			const input = fs.readFileSync(
-				tmpFile,
-				process.env.CSV_PARSE_ENCODING || "utf8"
-			);
+			const input = fs.readFileSync(tmpFile);
 
 			/* Parsing csv file */
 			const records = parse(input, {
-				columns: process.env.CSV_PARSE_COLUMNS === "true" || true,
+				columns: JSON.parse(process.env.CSV_PARSE_COLUMNS) || true,
 				comment: process.env.CSV_PARSE_COMMENT || "",
 				delimiter: process.env.CSV_PARSE_DELIMETER || ",",
 				encoding: process.env.CSV_PARSE_ENCODING || "utf8",
 				escape: process.env.CSV_PARSE_ESCAPE || '"',
 				quote: process.env.CSV_PARSE_QUOTE || '"',
-				record_delimiter: [],
+				record_delimiter:
+					JSON.parse(process.env.CSV_PARSE_RECORD_DELIMITER) || [],
 				skip_empty_lines:
-					process.env.CSV_PARSE_SKIP_EMPTY_LINES === "true" || true
+					JSON.parse(process.env.CSV_PARSE_SKIP_EMPTY_LINES) || true
 			});
 
 			/* Delete temporary directory */
 			fs.rmdirSync(tmpDir, { recursive: true });
 
+			const userIdLabel = process.env.CSV_COLUMN_USER_ID || "user_id";
+			const campaignIdLabel =
+				process.env.CSV_COLUMN_CAMPAIGN_ID || "campaign_id";
+
 			const notificationParamsColumns = Object.keys(records[0]).filter(
 				column => {
-					return (
-						column !== (process.env.CSV_COLUMN_USER_ID || "user_id") &&
-						column !== (process.env.CSV_COLUMN_CAMPAIGN_ID || "campaign_id")
-					);
+					return column !== userIdLabel && column !== campaignIdLabel;
 				}
 			);
 
-			/* Create query */
-			const query = {};
-
-			query.accessToken = process.env.WP_ACCESS_TOKEN;
-
-			query.targetUserIds = records.map(
-				item => item[process.env.CSV_COLUMN_USER_ID || "user_id"]
-			);
-			query.campaignId =
-				records[0][process.env.CSV_COLUMN_CAMPAIGN_ID || "campaign_id"];
-
-			if (notificationParamsColumns.length > 0) {
-				query.notificationParams = records.map(row => {
-					const customParams = {};
-
-					notificationParamsColumns.forEach(parameter => {
-						customParams[parameter] = row[parameter];
-					});
-
-					return customParams;
-				});
-			}
-
-			/* Slice query*/
+			/* Create queries */
 			const maxDeliveries =
 				Number(process.env.WP_MAXIMUM_DELIVERIES_TARGETS) || 10000;
 
 			const numberOfQueries = Math.ceil(
-				query.targetUserIds.length / maxDeliveries
+				records.map(item => item[userIdLabel]).length / maxDeliveries
 			);
 
 			// range of the query
@@ -89,26 +65,40 @@ const parseDataFromCsv = async (sftpConfig, remotePath) => {
 			let endIndex = maxDeliveries;
 
 			for (let i = 0; i < numberOfQueries; i++) {
-				const tmpQuery = { data: {}, range: {} };
+				const tmpQuery = {
+					data: {
+						accessToken: "",
+						targetUserIds: [],
+						campaignId: "",
+						notificationParams: []
+					},
+					range: {}
+				};
 
-				tmpQuery.data.accessToken = query.accessToken;
-				tmpQuery.data.targetUserIds = query.targetUserIds.slice(
-					startIndex,
-					endIndex
-				);
+				tmpQuery.data.accessToken = process.env.WP_ACCESS_TOKEN;
+				tmpQuery.data.targetUserIds = records
+					.map(item => item[userIdLabel])
+					.slice(startIndex, endIndex);
 
-				tmpQuery.data.campaignId = query.campaignId;
+				tmpQuery.data.campaignId = records[0][campaignIdLabel];
 
-				if (query.notificationParams.length > 0) {
-					tmpQuery.data.notificationParams = query.notificationParams.slice(
-						startIndex,
-						endIndex
-					);
+				if (notificationParamsColumns) {
+					tmpQuery.data.notificationParams = records
+						.map(row => {
+							const customParams = {};
+
+							notificationParamsColumns.forEach(parameter => {
+								customParams[parameter] = row[parameter];
+							});
+
+							return customParams;
+						})
+						.slice(startIndex, endIndex);
 				}
 
 				tmpQuery.range = {
 					fromLine: startIndex + 1,
-					toLine: Math.min(endIndex, query.notificationParams.length)
+					toLine: startIndex + tmpQuery.data.notificationParams.length
 				};
 
 				queriesArray.push(tmpQuery);
